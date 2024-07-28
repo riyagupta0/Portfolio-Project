ce3dce8f177d79eea2f19fc443b29463b1a7fbb6
riyagupta <guptariya9412@gmail.com>
2024-07-29 00:55:21 +0530

    updation

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -42,10 +42,10 @@ function EmailSection() {
     <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4' id="contact">
         
         <div>
-            <h5 className='text-xl font-bold text-white my-2'>Let's Connect</h5>
+            <h5 className='text-xl font-bold text-white my-2'>Let&apos;s Connect</h5>
             <p className='text-[#ADB7BE] mb-4 max-w-md'>
                 {" "}
-                I'm currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you!
+                I&apos;m currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I&apos;ll try my best to get back to you!
             </p>
             <div className='socials flex flex-row gap-2 '>
                 <Link href="github.com">
