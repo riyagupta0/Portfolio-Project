dd7d8fa673103876d6ae8b8a49fdfbd5988c8799
riyagupta <guptariya9412@gmail.com>
2024-07-29 00:23:48 +0530

    update image alt

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -74,7 +74,7 @@ const AboutSection = ()  => {
     <section id='about' className='text-white'>
         <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
             <Image src="/images/about-image.png" width={500} height={500} />
-            <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
+            <div className='mt-4 md:mt-0 text-left flex flex-col h-full' alt="about">
                 <h2 className='text-4xl font-bold text-white mb-4 '>About Me</h2>
                 <p className='text-base lg:text-lg'>I'm an MCA student with a strong grasp of programming languages and data structures. Passionate about problem-solving and technology, I'm eager to expand my skills in software development. With proficiency in HTML,CSS, Javascript, React.js , Node.js and C++ for DSA , I'm driven to innovate and make a meaningful impact in the tech industry.
 
